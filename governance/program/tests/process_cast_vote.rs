68183b1b9d874176b7bff2cc59a653c17087d394
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-13 10:54:03 +0200

    Test for casting vote with delegate

--- governance/program/tests/process_cast_vote.rs
+++ governance/program/tests/process_cast_vote.rs
@@ -2,15 +2,16 @@
 
 mod program_test;
 
-use solana_program::pubkey::Pubkey;
+use solana_program::{instruction::AccountMeta, pubkey::Pubkey};
 use solana_program_test::tokio;
 
 use program_test::*;
+use solana_sdk::signature::{Signer};
 use spl_governance::{
     error::GovernanceError,
     state::{
         enums::{MintMaxVoterWeightSource, ProposalState, VoteThreshold, VoteTipping},
-        vote_record::Vote,
+        vote_record::{Vote, VoteChoice},
     },
 };
 
@@ -1567,4 +1568,110 @@ async fn test_cast_vote_with_strict_tipping_and_inflated_max_vote_weight() {
     assert_eq!(ProposalState::Succeeded, proposal_account.state);
     // max_vote_weight should be coerced from 60 to 100
     assert_eq!(proposal_account.max_vote_weight, Some(100))
+}
+
+#[tokio::test]
+async fn test_cast_vote_with_delegate() {
+    // Arrange
+    let mut governance_test = GovernanceProgramTest::start_new().await;
+    let realm_cookie = governance_test.with_realm().await;
+    let governed_account_cookie = governance_test.with_governed_account().await;
+
+    let mut token_owner_record_cookie = governance_test
+        .with_community_token_deposit(&realm_cookie)
+        .await
+        .unwrap();
+
+    let mut governance_cookie = governance_test
+        .with_governance(
+            &realm_cookie,
+            &governed_account_cookie,
+            &token_owner_record_cookie,
+        )
+        .await
+        .unwrap();
+
+    let proposal_cookie = governance_test
+        .with_signed_off_proposal(&token_owner_record_cookie, &mut governance_cookie)
+        .await
+        .unwrap();
+
+    // set the delegate
+    governance_test
+        .with_community_governance_delegate(&realm_cookie, &mut token_owner_record_cookie)
+        .await;
+
+    let clock = governance_test.bench.get_clock().await;
+
+    // Act - casting Yes/No vote with a delegate
+    let vote = Vote::Approve(vec![VoteChoice {
+        rank: 0,
+        weight_percentage: 100,
+    }]);
+    let vote_record_cookie = governance_test
+        .with_cast_vote_using_instruction(
+            &proposal_cookie,
+            &token_owner_record_cookie,
+            vote,
+            // changing the cast vote to use delegate - see GovernanceInstruction::CastVote
+            |itx| {
+                itx.accounts[5] = AccountMeta::new_readonly(
+                    token_owner_record_cookie.governance_delegate.pubkey(),
+                    true,
+                )
+            },
+            Some(&[&token_owner_record_cookie.governance_delegate]),
+        )
+        .await.unwrap();
+
+    // Assert
+    // was the delegate set up for the proposal
+    let token_owner_record = governance_test
+        .get_token_owner_record_account(&token_owner_record_cookie.address)
+        .await;
+    assert_eq!(
+        Some(token_owner_record_cookie.governance_delegate.pubkey()),
+        token_owner_record.governance_delegate
+    );
+
+    let vote_record_account = governance_test
+        .get_vote_record_account(&vote_record_cookie.address)
+        .await;
+    assert_eq!(vote_record_cookie.account, vote_record_account);
+
+    let proposal_account = governance_test
+        .get_proposal_account(&proposal_cookie.address)
+        .await;
+    assert_eq!(
+        token_owner_record_cookie
+            .account
+            .governing_token_deposit_amount,
+        proposal_account.options[0].vote_weight
+    );
+    assert_eq!(proposal_account.state, ProposalState::Succeeded);
+    assert_eq!(
+        proposal_account.voting_completed_at,
+        Some(clock.unix_timestamp)
+    );
+    assert_eq!(Some(100), proposal_account.max_vote_weight);
+    assert_eq!(
+        Some(governance_cookie.account.config.community_vote_threshold),
+        proposal_account.vote_threshold
+    );
+
+    let token_owner_record = governance_test
+        .get_token_owner_record_account(&token_owner_record_cookie.address)
+        .await;
+    assert_eq!(1, token_owner_record.unrelinquished_votes_count);
+    assert_eq!(1, token_owner_record.total_votes_count);
+
+    let realm_account = governance_test
+        .get_realm_account(&realm_cookie.address)
+        .await;
+    assert_eq!(0, realm_account.voting_proposal_count);
+
+    let governance_account = governance_test
+        .get_governance_account(&governance_cookie.address)
+        .await;
+    assert_eq!(0, governance_account.voting_proposal_count);
 }
